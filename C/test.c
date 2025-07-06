3c5a03c3e6eb46770c9cb708815e0228ae27637f
cr1ptondev <weasfear@mail.ru>
2025-07-06 16:28:05 +0300

    update 6.July

--- C/test.c
+++ C/test.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main(){
-    printf("Size of char: %lu bytes\n", sizeof(char));
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of pointer: %lu bytes\n", sizeof(void *));
-    return 0;
+// int main(){
+//     printf("Size of char: %lu bytes\n", sizeof(char));
+//     printf("Size of int: %lu bytes\n", sizeof(int));
+//     printf("Size of float: %lu bytes\n", sizeof(float));
+//     printf("Size of double: %lu bytes\n", sizeof(double));
+//     printf("Size of pointer: %lu bytes\n", sizeof(void *));
+//     return 0;
+// }
+
+int main(void) {
+    int a = 1;
+    printf("%d", a);
+
+    int two(void) {
+	printf("What is your name\?\n\tMy name\'s Bond! James Bond!\n");
+	return 0;
+}
+return 0;
 }
